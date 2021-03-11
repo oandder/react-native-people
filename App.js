9cd017ab46a1f82ed48b5298f411aa4f02888eda
Anderson Menezes <oanderson.design@gmail.com>
2021-03-11 14:10:35 -0300

    First commit

--- App.js
+++ App.js
@@ -2,6 +2,8 @@ import React from 'react'
 import { View } from 'react-native'
 import Header from './src/components/Header'
 
+
+
 export default class App extends React.Component {
   render() {
     return (
